c6aca48d9f4cfafecbf8e30f28192ceca3e34596
ayoublaroussi <ayoublaroussipro@gmail.com>
2024-11-09 18:36:15 +0100

    feat(main.java): exo poo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // compterString();
+        afficherMinEtMaj();
+    }
+    static void compterString(){
         String result;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Saisissez un mot :");
+        System.out.println("Laissez un mot :");
         result = scanner.nextLine();
+        System.out.println(result.length());
+    }
+    static void afficherMinEtMaj(){
+        String result;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Laissez un mot :");
+        result = scanner.nextLine();
+        System.out.println(result.toLowerCase());
+        System.out.println(result.toUpperCase());
 
-        int count = 0;
-        for (int i = 1; i < result.length(); i++) {
-            count =+ i;
-        }
-        System.out.print(count);
     }
 }
